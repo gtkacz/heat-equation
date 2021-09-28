418ffa6097debc49eda1d28d8d7ddf21e4519f15
Tkacz <gabrielmt2@al.insper.edu.br>
2021-09-28 17:46:21 -0300

    att

--- 2d.py
+++ 2d.py
@@ -14,23 +14,40 @@ def main():
     L = 0.5
     t_f = 10
     
-    Tcentro = 100
-    
     x = np.arange(0, L + Δx, Δx)
     
     linhas = colunas = len(x)
     n = int(t_f/Δt)
     F0 = 25
     
-    l = np.zeros((n, linhas, colunas))
-    l[0:n][0][1:colunas-1] = Tcentro
+    # Initialize solution: the grid of l(k, i, j)
+    l = np.empty((n, linhas, colunas))
+
+    # Initial condition everywhere inside the grid
+    u_initial = 0
+
+    # Boundary conditions
+    u_top = 100.0
+    u_left = 75.0
+    u_bottom = 0.0
+    u_right = 50.0
+
+    # Set the initial condition
+    l.fill(u_initial)
+
+    # Set the boundary conditions
+    l[:, (linhas-1):, :] = u_top
+    l[:, :, :1] = u_left
+    l[:, :1, 1:] = u_bottom
+    l[:, :, (colunas-1):] = u_right
     
-    for k in range(0, t_f-1, 1):
-        for i in np.arange(1, L - 0.1, Δx):
-            for j in np.arange(1, L - 0.1, Δx):
-                l[k + 1, i, j] = (l[k][i+1][j] + l[k][i-1][j] + l[k][i][j+1] + l[k][i][j-1] - 4*l[k][i][j]) + l[k][i][j]
-                
+    for k in range(1, n):
+        for i in range(1, linhas-1):
+            for j in range(1, colunas-1):
+                l[k, i, j] = l[k-1, i, j] + F0*(l[k-1, i+1, j] + l[k-1, i-1, j] + l[k-1, i, j+1] + l[k-1, i, j-1] + -4*l[k-1, i, j])
+    
     sns.heatmap(l[-1])
+    plt.show()
 
 if __name__ == '__main__':
     main()
